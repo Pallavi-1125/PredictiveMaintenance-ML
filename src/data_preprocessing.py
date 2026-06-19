4a49486190e733c36100041a9ad477df4ad731aa
Pallavi-1125 <laxmipallavi24@gmail.com>
2026-06-19 21:37:31 +0530

    feat: added initial data preprocessing script

---
+++ src/data_preprocessing.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Load dataset
+df = pd.read_csv("data/predictive_maintenance.csv")
+
+# Show first 5 rows
+print(df.head())
+
+# Check missing values
+print(df.isnull().sum())
+
+# Basic information
+print(df.info())
